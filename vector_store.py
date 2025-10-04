3e45177f2eb7f0916bf43bd4fa192cdd16a340af
Avantika <123316093+AvantikaSharma2307@users.noreply.github.com>
2025-10-04 19:31:56 +0530

    API_CREATED

--- vector_store.py
+++ vector_store.py
@@ -1,9 +1,11 @@
 # vector_store.py
 
-import faiss
+import faiss # type: ignore
 import os
 import json
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer # type: ignore
+from datetime import datetime
+
 
 VECTOR_FOLDER = "faiss_store"
 os.makedirs(VECTOR_FOLDER, exist_ok=True)
@@ -26,16 +28,23 @@ def save_vector_db():
     with open(DATA_FILE, "w") as f:
         json.dump(metadata, f, indent=2)
 
-def add_invoice_to_db(json_data, filename):
+def add_invoice_to_db(json_data, filename,tenant_id,location):
     # Convert the entire invoice JSON to a single string for embedding
     full_text = json.dumps(json_data, indent=2)
 
     embedding = MODEL.encode([full_text])[0]  # Generate vector
     index.add(embedding.reshape(1, -1))
 
+
+    
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     # Store metadata
     metadata.append({
         "filename": filename,
+        "tenant_id": tenant_id,
+        "location": location,
+        "timestamp": timestamp,
         "data": json_data,
         "text": full_text
     })
